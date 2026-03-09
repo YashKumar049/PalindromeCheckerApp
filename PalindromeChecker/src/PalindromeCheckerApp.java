c69ecd95348e52cb83be086ba51898e81d249131
Chandan <shulabhproperties@gmail.com>
2026-03-09 18:42:40 +0530

    UC6 - Palindrome using Stack and Queue

--- PalindromeChecker/src/PalindromeCheckerApp.java
+++ PalindromeChecker/src/PalindromeCheckerApp.java
@@ -1,48 +1,41 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 import java.util.Stack;
 
 public class PalindromeCheckerApp {
-
     public static void main(String[] args) {
-
-        // UC1: Welcome Message
-        System.out.println("========================================");
-        System.out.println("Welcome to Palindrome Management System");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized Successfully done");
-        System.out.println("========================================");
-
-        // UC2: User Input
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a word to check: ");
+
+        System.out.print("Input text: ");
         String input = scanner.nextLine();
 
-        String cleanInput = input.toLowerCase();
+        boolean result = isPalindrome(input);
 
-        // UC5: Stack Based Palindrome Check
-        Stack<Character> stack = new Stack<>();
-
-        // push characters
-        for(char c : cleanInput.toCharArray()){
-            stack.push(c);
-        }
-
-        // pop characters to make reverse
-        StringBuilder reversed = new StringBuilder();
-
-        while(!stack.isEmpty()){
-            reversed.append(stack.pop());
-        }
-
-        System.out.println("Stack reversed string: " + reversed);
-
-        // comparison
-        if(cleanInput.equals(reversed.toString())){
-            System.out.println("The string \"" + input + "\" IS a palindrome.");
-        } else {
-            System.out.println("The string \"" + input + "\" is NOT a palindrome.");
-        }
+        System.out.println("Is it a Palindrome? : " + result);
 
         scanner.close();
+    }
+
+    public static boolean isPalindrome(String input) {
+        if (input == null || input.isEmpty()) return false;
+
+        String clean = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
+
+        for (char c : clean.toCharArray()) {
+            stack.push(c);
+            queue.add(c);
+        }
+
+        while (!stack.isEmpty()) {
+            if (!stack.pop().equals(queue.poll())) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
