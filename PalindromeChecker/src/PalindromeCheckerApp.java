6f8a42d86bf5b60324d21755ecb6427abdb14884
Chandan <shulabhproperties@gmail.com>
2026-03-09 19:13:13 +0530

    UC11 - Ignore spaces using recursion

--- PalindromeChecker/src/PalindromeCheckerApp.java
+++ PalindromeChecker/src/PalindromeCheckerApp.java
@@ -1,37 +1,50 @@
-import java.util.LinkedList;
 import java.util.Scanner;
 
 public class PalindromeCheckerApp {
+
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Input text: ");
+        System.out.println("===== Palindrome Checker (UC11 - Ignore Spaces) =====");
+
+        System.out.print("Enter text: ");
         String input = scanner.nextLine();
 
         boolean result = isPalindrome(input);
 
-        System.out.println("Is it a Palindrome? : " + result);
+        if(result){
+            System.out.println("The string \"" + input + "\" IS a palindrome.");
+        } else {
+            System.out.println("The string \"" + input + "\" is NOT a palindrome.");
+        }
 
         scanner.close();
     }
 
+    // Method to prepare string
     public static boolean isPalindrome(String input) {
-        if (input == null || input.isEmpty()) return false;
 
+        if (input == null || input.isEmpty()) {
+            return false;
+        }
+
+        // Remove spaces and convert to lowercase
         String clean = input.replaceAll("\\s+", "").toLowerCase();
 
-
         return checkPalindrome(clean, 0, clean.length() - 1);
-
-
     }
 
+    // Recursive palindrome check
     public static boolean checkPalindrome(String str, int start, int end) {
-        if (start >= end)
-            return true;
 
-        if (str.charAt(start) != str.charAt(end))
+        if (start >= end) {
+            return true;
+        }
+
+        if (str.charAt(start) != str.charAt(end)) {
             return false;
+        }
 
         return checkPalindrome(str, start + 1, end - 1);
     }
